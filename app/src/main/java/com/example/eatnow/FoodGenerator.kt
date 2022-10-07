4f25d106f32b5b6297953b05119fb5f255b951f7
Mahdi <MahdiPorkar2002@gmail.com>
2022-10-07 03:25:31 -0700

    created object class for data of recyclerView

---
+++ app/src/main/java/com/example/eatnow/FoodGenerator.kt
@@ -0,0 +1,185 @@
+package com.example.eatnow
+
+object FoodGenerator {
+
+    fun getFoods(): ArrayList<Food> {
+        return arrayListOf(
+            Food(
+                "https://www.carpediemrome.com/wp-content/uploads/2022/03/Authentic-Italian-Pizza-Margherita.-Photo-by-Aurelien-Lemasson-Theobald.jpg",
+                "pizza",
+                "Rome",
+                "$25",
+                "1.6 miles from you",
+                4.7f,
+                1500
+            ),
+            Food(
+                "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Crispy-Fried-Chicken_EXPS_TOHJJ22_6445_DR%20_02_03_11b.jpg",
+                "Fried Chicken",
+                "New York",
+                "$15",
+                "3.6 miles from you",
+                4.1f,
+                500
+            ),
+
+            Food(
+                "https://cdn.britannica.com/52/128652-050-14AD19CA/Maki-zushi.jpg",
+                "Sushi",
+                "Tokyo",
+                "$13",
+                "2.6 miles from you",
+                3.9f,
+                700
+            ),
+            Food(
+                "https://hipersia.com/en/mi_ax/Original/1398/10/3719.jpg",
+                "Kebab",
+                "Bonab",
+                "$33",
+                "2.6 miles from you",
+                4.9f,
+                900
+            ),
+            Food(
+                "https://hipersia.com/en/mi_ax/Original/1398/10/3719.jpg",
+                "Caesar Salad",
+                "Mexico",
+                "$9",
+                "2.0 miles from you",
+                4.2f,
+                300
+            ),
+
+            Food(
+                "https://cdn.britannica.com/31/122031-050-F8FCA663/Hamburger-cheeseburger.jpg",
+                "Hamburger",
+                "Mexico",
+                "$19",
+                "3.2 miles from you",
+                4.6f,
+                1200
+            ),
+            Food(
+                "https://ecom.daranoconsulting.com/wp-content/uploads/2021/05/Lasagna-i.jpg",
+                "Lazania",
+                "Naples",
+                "$19",
+                "3.2 miles from you",
+                3.6f,
+                200
+            ),
+            Food(
+                "https://panamag.ir/wp-content/uploads/2021/10/koufte-tabrizi.jpg",
+                "Kufte",
+                "Tabriz",
+                "$10",
+                "2.2 miles from you",
+                3.8f,
+                200
+            ),
+            Food(
+                "Hamburger",
+                "15",
+                "3",
+                "Isfahan, Iran",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food1.jpg",
+                4.5f,
+                20
+            ),
+            Food(
+                "Grilled fish",
+                "20",
+                "2.1",
+                "Tehran, Iran",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food2.jpg",
+                4f,
+                10
+            ),
+            Food(
+                "Lasania",
+                "40",
+                "1.4",
+                "Isfahan, Iran",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food3.jpg",
+                2f,
+                30
+            ),
+            Food(
+                "pizza",
+                "10",
+                "2.5",
+                "Zahedan, Iran",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food4.jpg",
+                1.5f,
+                80
+            ),
+            Food(
+                "Roasted Fish",
+                "40",
+                "3.7",
+                "Jolfa, Iran",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food6.jpg",
+                3.2f,
+                50
+            ),
+            Food(
+                "Fried chicken",
+                "70",
+                "3.5",
+                "NewYork, USA",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food7.jpg",
+                2.5f,
+                34
+            ),
+            Food(
+                "Vegetable salad",
+                "12",
+                "3.6",
+                "Berlin, Germany",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food8.jpg",
+                4.5f,
+                62
+            ),
+            Food(
+                "Grilled chicken",
+                "10",
+                "3.7",
+                "Beijing, China",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food9.jpg",
+                4f,
+                60
+            ),
+            Food(
+                "Baryooni",
+                "16",
+                "10",
+                "Ilam, Iran",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food10.jpg",
+                4.5f,
+                28
+            ),
+            Food(
+                "Ghorme Sabzi",
+                "11.5",
+                "7.5",
+                "Karaj, Iran",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food11.jpg",
+                5f,
+                27
+            ),
+            Food(
+                "Rice",
+                "12.5",
+                "2.4",
+                "Shiraz, Iran",
+                "https://dunijet.ir/YaghootAndroidFiles/DuniFoodSimple/food12.jpg",
+                2.5f,
+                50
+            ),
+
+
+            )
+    }
+
+}
