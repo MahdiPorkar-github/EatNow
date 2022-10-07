b208d4dd62b1e1a11c52d4ea1f774c73e3d3ffff
Mahdi <MahdiPorkar2002@gmail.com>
2022-10-07 02:36:53 -0700

    created recyclerView data class

---
+++ app/src/main/java/com/example/eatnow/Food.kt
@@ -0,0 +1,11 @@
+package com.example.eatnow
+
+class Food(
+    val imageUrl: String,
+    val name: String,
+    val city: String,
+    val price: String,
+    val distance: String,
+    val rating: Float,
+    val ratersCount: Int
+)
